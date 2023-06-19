bf5cf8d8c3bc3fe60c77f0740d20bb4907d4c65b
Anurag Singh <anurag.singh2_cs20@gla.ac.in>
2023-06-19 18:17:17 +0530

    Initial

--- src/InsertAtBegning.java
+++ src/InsertAtBegning.java
@@ -1,2 +1,25 @@
 public class InsertAtBegning {
+    public static Node insertAtBegning(Node head,int d){
+        Node temp=new Node(d);
+        temp.next=head;
+        head=temp;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Node head=null;
+        head=insertAtBegning(head,10);
+        head=insertAtBegning(head,20);
+        head=insertAtBegning(head,30);
+//        System.out.println(head.data);
+        print(head);
+
+    }
+    public static void print(Node head){
+        if(head==null){
+            return;
+        }
+        System.out.print(head.data+" ");
+        print(head.next);
+    }
 }
